d9d93f0633420952d000268caf6c78e0bdeaed9a
Alice Chen <achen24@charlotte.edu>
2025-01-26 23:15:28 -0500

    solution to merge sort assignment

--- merge-sort-activity/mergesort.cpp
+++ merge-sort-activity/mergesort.cpp
@@ -8,11 +8,15 @@ int* merge(int* leftSide, int leftSize, int* rightSide, int rightSize);
 
 int main() {
     int size;
+    //change seed to current time
     srand(time(0));
-    //create array of random integers
-    std::cout << "Enter size of array: " << std::endl;
-    std::cin >> size;
+    
+    std::cout << "Size of array: " << std::endl;
+    //make size between 1 and 10
+    size = rand() % 10 + 1;
+    std::cout << size << std::endl;
 
+    //create array of random ints
     int* array = arrayRandomizer(size);
     std::cout << "Unsorted array: " << std::endl;
     for (int i = 0; i < size; i++) {
@@ -20,6 +24,7 @@ int main() {
     }
     std::cout << std::endl;
 
+    //sort array
     std::cout << "Sorted array: " << std::endl;
     int* sortedArray = mergeSort(array, size);
     for (int i = 0; i < size; i++) {
@@ -27,6 +32,7 @@ int main() {
     }
     std::cout << std::endl;
 
+    //delete arrays
     delete[] array;
     delete[] sortedArray;
     return 0;
@@ -41,13 +47,14 @@ int* arrayRandomizer(int size) {
 }
 
 int* mergeSort(int* array, int size) {
+    //base case
     if (size == 1) {
         int* newArray = new int[1];
         newArray[0] = array[0];
         return newArray;
     }
     int middle = size / 2;
-
+    //allocate new arrays
     int* leftSide = new int[middle];
     for (int i = 0; i < middle; i++) {
         leftSide[i] = array[i];
@@ -57,7 +64,7 @@ int* mergeSort(int* array, int size) {
     for (int i = middle; i < size; i++) {
         rightSide[i - middle] = array[i];
     }
-
+    //recursive call
     int* sortedLeftSide = mergeSort(leftSide, middle);
     int* sortedRightSide = mergeSort(rightSide, size - middle);
 
@@ -69,7 +76,7 @@ int* mergeSort(int* array, int size) {
 int* merge(int* leftSide,int leftSize, int* rightSide, int rightSize) {
     int* newArray = new int[leftSize + rightSize];
     int a = 0, b = 0, c = 0;
-
+    //while both arrays have elements, add the smaller element to the new array
     while (a < leftSize && b < rightSize) {
         if (leftSide[a] <= rightSide[b]) {
             newArray[c] = leftSide[a]; 
@@ -81,6 +88,7 @@ int* merge(int* leftSide,int leftSize, int* rightSide, int rightSize) {
             b++;
         }
     }
+    //add remaining elements
     while (a < leftSize) {
         newArray[c] = leftSide[a];
         c++;
