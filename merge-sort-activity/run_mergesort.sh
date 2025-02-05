57732828eba22f3e2c05a72020f79cf235acec83
Alice Chen <achen24@charlotte.edu>
2025-02-04 23:48:57 -0500

    updating script

--- merge-sort-activity/run_mergesort.sh
+++ merge-sort-activity/run_mergesort.sh
@@ -11,9 +11,9 @@ echo "size,time" > $output_file
 for size in 10 100 1000 10000 100000 1000000 10000000 100000000 1000000000
 do 
     echo "Running for size: $size"
-    start=$(date +%s)
+    start=$(date +%s%3N)
    ./mergesort $size
-    end=$(date +%s)
+    end=$(date +%s%3N)
     runtime=$((end - start))
     echo "$size,$runtime" >> $output_file
 done
