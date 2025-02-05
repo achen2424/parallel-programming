21a8467d6cf491cf64898c642a63ed89593a5b56
Alice Chen <achen24@charlotte.edu>
2025-02-04 23:34:07 -0500

    updating script

--- merge-sort-activity/run_mergesort.sh
+++ merge-sort-activity/run_mergesort.sh
@@ -4,7 +4,10 @@
 #SBATCH --time=01:00:00
 #SBATCH --mem=32G
 
-output_file = "mergesort_output.csv"
+output_file="mergesort_output.csv"
+
+echo "size,time" > $output_file
+
 for size in 10 100 1000 10000 100000 1000000 10000000 100000000 1000000000
 do 
     echo "Running for size: $size"
@@ -12,6 +15,7 @@ do
    ./mergesort $size
     end=$(date +%s)
     runtime=$((end - start))
-    echo "Size: $size, Runtime: $runtime seconds" >> $output_file
+    echo "$size,$runtime" >> $output_file
 done
+
 echo "Done!"
