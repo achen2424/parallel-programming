55174070f8a9a2040b28890be58a6f8029ca4c58
Alice Chen <achen24@charlotte.edu>
2025-02-05 18:50:59 -0500

    added python plot file and updated Makefile

--- merge-sort-activity/plot.py
+++ merge-sort-activity/plot.py
@@ -1,9 +1,24 @@
 import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd
 
-x, y = np.loadtxt('mergesort_output.csv', delimiter=',', unpack=True)
-plt.xlabel('Size of Array')
-plt.ylabel('Time (microseconds)')
-plt.title('Time Complexity of Merge Sort')
-plt.plot(x, y)
+data = pd.read_csv("mergesort_output.csv")
+
+sizes = data["size"]
+times = data["time(microseconds)"]
+
+plt.figure(figsize=(8, 6))
+plt.plot(sizes, times, "o-")
+
+plt.xscale("log")
+plt.yscale("log")
+
+plt.xticks(sizes)
+plt.yticks(times, times)
+
+plt.xlabel("Number of elements")
+plt.ylabel("Time (microseconds)")
+plt.title("Time Complexity of Merge Sort")
+plt.grid()
+
+plt.savefig("plot.png")
 plt.show()
