0f7964675e53202e6a05a581973a0a9d19d43b7d
Alice Chen <achen24@charlotte.edu>
2025-02-19 00:06:34 -0500

    added script

--- nbody/simulate.sh
+++ nbody/simulate.sh
@@ -4,14 +4,13 @@
 #SBATCH --time=01:00:00
 #SBATCH --mem=32G
 
-output_file="simulation_times.tsv"
+output_file="simulation_times.csv"
 
-echo "5000000 steps, 200 dt, 9 particles" >> $output_file
-# 8 planets and the sun
-./nbody 5000000 200 9 >> $output_file
+echo "9 particles (sun and 8 planets), 200 dt, 5000000 steps, 1000 log interval" >> $output_file
+./nbody 9 200 5000000 10000 >> $output_file
 
-echo "10000 steps, 1 dt, 100 particles" >> $output_file
-./nbody 10000 1 100 >> $output_file
+echo "100 particles, 1 dt, 10000 steps, 100 log interval" >> $output_file
+./nbody 100 1 10000 100 >> $output_file
 
-echo "10000 steps, 1 dt, 1000 particles" >> $output_file
-./nbody 10000 1 1000 >> $output_file
+echo "1000 particles, 1 dt, 10000 steps, 500 log interval" >> $output_file
+./nbody 1000 1 10000 500 >> $output_file
