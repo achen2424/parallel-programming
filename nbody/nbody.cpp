3bbd00d900ae08cfff35ca153e65f30bd827f763
Alice Chen <achen24@charlotte.edu>
2025-02-19 00:05:53 -0500

    fixed randomization of particles and force calculation

--- nbody/nbody.cpp
+++ nbody/nbody.cpp
@@ -4,37 +4,62 @@
 #include <fstream>
 #include <chrono>
 #include <cmath>
+#include <random>
 
 using namespace std;
+const double G = 6.674e-11;
 
 struct Particle {
     double mass;
     double x, y, z, vx, vy, vz, fx, fy, fz;
+    Particle() : mass(0), x(0), y(0), z(0), vx(0), vy(0), vz(0), fx(0), fy(0), fz(0) {}
+    Particle(double mass, double x, double y, double z, double vx, double vy, double vz, double fx, double fy, double fz)
+        : mass(mass), x(x), y(y), z(z), vx(vx), vy(vy), vz(vz), fx(fx), fy(fy), fz(fz) {}
 };
 
 //initialize particles with random position & velocity
 void initialize(vector<Particle>& particles, int nbpart) {
-    for (int i = 0; i < nbpart; i++) {
-        Particle p;
-        p.mass = 1.0;
-        p.x = rand() % 100;
-        p.y = rand() % 100;
-        //using x-y plane
-        p.z = 0;
-        p.vx = rand() % 100;
-        p.vy = rand() % 100;
-        p.vz = 0;
-        p.fx = 0.0;
-        p.fy = 0.0;
-        p.fz = 0.0;
-        particles.push_back(p);
+    particles.resize(nbpart);
+
+    if (nbpart == 2) {  //sun at the center and earth
+        particles[0] = Particle(1.95e30, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        particles[1] = Particle(5.9e24, 1.5e11, 0, 0, 0, 29800, 0, 0, 0, 0); 
+    } 
+    else {
+        //sun at the center
+        particles[0] = Particle(1.95e30, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        //randomly generate particles
+        random_device rd;
+        //random number generator
+        mt19937 gen(rd());
+        //random x, y, mass
+        uniform_real_distribution<double> dist_x(-5e11, 5e11);
+        uniform_real_distribution<double> dist_y(-5e11, 5e11);
+        uniform_real_distribution<double> mass_dist(1e20, 1e25);
+
+        for (int i = 1; i < nbpart; i++) {
+            double x = dist_x(gen);
+            double y = dist_y(gen);
+            //2d
+            double z = 0;
+
+            double distance = sqrt(x * x + y * y);
+            double orbital_speed = sqrt(G * particles[0].mass / distance);
+            double vx = -orbital_speed * (y / distance);
+            double vy = orbital_speed * (x / distance);
+            double vz = 0;
+            double mass = mass_dist(gen);
+
+            particles[i] = Particle(mass, x, y, z, vx, vy, vz, 0, 0, 0);
+        }
     }
 }
 
+
 //calculate gravitational forces
 void computeForces(vector<Particle>& particles) {
-    const double G = 6.674e-11;
-    const double softening = 1e-10;
+    //prevent division by 0
+    const double softening = 1e9;
     //forces reset to 0
     for(size_t i = 0; i < particles.size(); i++) {
         particles[i].fx = 0.0;
@@ -50,10 +75,15 @@ void computeForces(vector<Particle>& particles) {
                 double distance = sqrt(distanceSq + softening);
                 //gravitational force magnitude
                 double force = G * particles[j].mass * particles[i].mass / distanceSq;
-                //force components added to each particle
-                particles[i].fx += dx * force / distance;
-                particles[i].fy += dy * force / distance;
-                particles[i].fz += dz * force / distance;
+                //force components
+                particles[i].fx += force * (dx  / distance);
+                particles[i].fy += force * (dy / distance);
+                particles[i].fz += force* (dz / distance);
+
+
+                particles[j].fx -= force * (dx  / distance);
+                particles[j].fy -= force * (dy / distance);
+                particles[j].fz -= force* (dz / distance);
             }
         }
     }
@@ -77,9 +107,9 @@ void update(vector<Particle>& particles, double dt) {
     }
 }
 
-//write particles to file
-void output(vector<Particle>& particles, int timeStep, ofstream& file) {
-    file << particles.size() << endl;
+//write state of particles to file
+void output(vector<Particle>& particles, ofstream& file) {
+    file << particles.size();
     for(size_t i = 0; i < particles.size(); i++) {
         file << "\t" << particles[i].mass << "\t" << particles[i].x << "\t" << particles[i].y << "\t" << particles[i].z;
         file << "\t" << particles[i].vx << "\t" << particles[i].vy << "\t" << particles[i].vz;
@@ -89,18 +119,17 @@ void output(vector<Particle>& particles, int timeStep, ofstream& file) {
 }
 
 int main(int argc, char* argv[]) {
-    int numParticles = 10;
-    double dt = 0.01;
-    int numIterations = 10000;
-    string outputFile = "nbody_output.tsv";
-
-    if (argc == 4) {
-        numParticles = atoi(argv[1]);
-        dt = atof(argv[2]);
-        numIterations = atoi(argv[3]);
-    } else {
-        cout << "Please enter the following arguments: ./nbody [numParticles] [dt] [numIterations]" << endl;
+    if (argc != 5) {
+        std::cerr << "Please provide 4 arguments: " << argv[0] << " num_particles time_step num_iterations log_interval" << std::endl;
+        return 1;
     }
+
+    //convert string to int
+    int numParticles = std::stoi(argv[1]);
+    double dt = std::stoi(argv[2]);
+    int numIterations = std::stoi(argv[3]);
+    int log_interval = std::stoi(argv[4]);
+    string outputFile = "solar.tsv";
     
     vector<Particle> particles;
     initialize(particles, numParticles);
@@ -111,7 +140,11 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < numIterations; i++) {
         computeForces(particles);
         update(particles, dt);
-        output(particles, i, file);
+
+        //dump state at every "log_interval" steps
+        if (i % log_interval == 0) {
+            output(particles, file);
+        }
     }
 
     auto stop = chrono::high_resolution_clock::now();
@@ -121,3 +154,7 @@ int main(int argc, char* argv[]) {
     file.close();
     return 0;
 }
+
+
+
+
