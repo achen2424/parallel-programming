10a890502c977de7302b5cb7937635d41e4e6772
Alice Chen <achen24@charlotte.edu>
2025-03-12 23:46:57 -0400

    fixed arguments

--- graph-crawler/gc.sh
+++ graph-crawler/gc.sh
@@ -6,8 +6,8 @@
 make
 
 #example tests
-./gc "Michael Schumacher" 2 > michael_schumacher_2.txt
-./gc "Tom Hanks" 3 > tom_hanks_3.txt
-./gc "Emma Watson" 1 > emma_watson_1.txt
+./gc Michael Schumacher 2 > michael_schumacher_2.txt
+./gc Tom Hanks 3 > tom_hanks_3.txt
+./gc Emma Watson 1 > emma_watson_1.txt
 
 echo "Test runs completed."
