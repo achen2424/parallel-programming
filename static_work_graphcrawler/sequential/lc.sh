c01e07d15acef1c5aedf7f76051f5896eb9d81ca
Alice Chen <achen24@charlotte.edu>
2025-04-02 23:56:47 -0400

    added script

---
+++ static_work_graphcrawler/sequential/lc.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --job-name=graph_crawler
+#SBATCH --partition=Centaurus
+#SBATCH --time=01:00:00
+#SBATCH --mem=32G
+make
+
+mkdir -p output
+
+#example tests
+./level_client "Michael Schumacher" 2 > output/michael_schumacher_2.txt
+./level_client "Tom Hanks" 1 > output/tom_hanks_1.txt
+./level_client "Viola Davis" 2 > output/viola_davis_2.txt
+
+echo "Test runs completed."
