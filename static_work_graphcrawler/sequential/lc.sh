482ee2130750d98854f7a10e0d75cd40e37c26f4
Alice Chen <achen24@charlotte.edu>
2025-04-04 14:02:20 -0400

    updated script

--- static_work_graphcrawler/sequential/lc.sh
+++ static_work_graphcrawler/sequential/lc.sh
@@ -8,8 +8,11 @@ make
 mkdir -p output
 
 #example tests
-./level_client "Michael Schumacher" 2 > output/michael_schumacher_2.txt
-./level_client "Tom Hanks" 1 > output/tom_hanks_1.txt
-./level_client "Viola Davis" 2 > output/viola_davis_2.txt
+./par_level_client "Michael Schumacher" 2 > output/PAR_michael_schumacher_2.txt
+./level_client "Michael Schumacher" 2 > output/SEQ_michael_schumacher_2.txt
+./par_level_client "Tom Hanks" 1 > output/PAR_tom_hanks_1.txt
+./level_client "Tom Hanks" 1 > output/SEQ_tom_hanks_1.txt
+./par_level_client "Viola Davis" 2 > output/PAR_viola_davis_2.txt
+./level_client "Viola Davis" 2 > output/SEQ_viola_davis_2.txt
 
 echo "Test runs completed."
