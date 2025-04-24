59b81035f57ff8494002d8b7591e349165fe5140
Alice Chen <achen24@charlotte.edu>
2025-04-23 23:17:52 -0400

    updated testcases.sh

--- nbody-par/sequential/testcases.sh
+++ nbody-par/sequential/testcases.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 #SBATCH --job-name=nbody-comparison
 #SBATCH --partition=Centaurus
-#SBATCH --time=00:30:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem=4G
+#SBATCH --time=01:00:00
+#SBATCH --mem=32G
 #SBATCH --output=output.txt
+#SBATCH --error=timing.txt
 
-#test case 1: 10 bodies, 1 step, 10,000 timesteps
-echo "Running parallel version for 10 bodies, 1 step, 10,000 timesteps"
-./nbody-par 10 1 10000 100
-echo "Running sequential version for 10 bodies, 1 step, 10,000 timesteps"
-./nbody-seq 10 1 10000 100
+echo "---Test Case 1: 10 bodies, 1 step, 10,000 timesteps---" >> timing.txt
+echo "Parallel version:" >> timing.txt
+./nbody-par 10 1 10000 100 > sim-par-1.out 2>> timing.txt
+echo "Sequential version:" >> timing.txt
+./nbody-seq 10 1 10000 100 > sim-seq-1.out 2>> timing.txt
+echo "" >> timing.txt
 
-#test case 2: 50 bodies, 1 step, 5000 timesteps
-echo "Running parallel version for 50 bodies, 1 step, 5000 timesteps"
-./nbody-par 50 1 5000 50
-echo "Running sequential version for 50 bodies, 1 step, 5000 timesteps"
-./nbody-seq 50 1 5000 50
+echo "---Test Case 2: 50 bodies, 1 step, 5,000 timesteps---" >> timing.txt
+echo "Parallel version:" >> timing.txt
+./nbody-par 50 1 5000 50 > sim-par-2.out 2>> timing.txt
+echo "Sequential version:" >> timing.txt
+./nbody-seq 50 1 5000 50 > sim-seq-2.out 2>> timing.txt
+echo "" >> timing.txt
 
-#test case 3: 100 bodies, 1 step, 2000 timesteps
-echo "Running parallel version for 100 bodies, 1 step, 2000 timesteps"
-./nbody-par 100 1 2000 20
-echo "Running sequential version for 100 bodies, 1 step, 2000 timesteps"
-./nbody-seq 100 1 2000 20
+echo "---Test Case 3: 100 bodies, 1 step, 2,000 timesteps---" >> timing.txt
+echo "Parallel version:" >> timing.txt
+./nbody-par 100 1 2000 20 > sim-par-3.out 2>> timing.txt
+echo "Sequential version:" >> timing.txt
+./nbody-seq 100 1 2000 20 > sim-seq-3.out 2>> timing.txt
