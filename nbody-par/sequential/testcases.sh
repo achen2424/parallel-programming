088af22bc30d8f51c95caf69e9baf1f08930bc88
Alice Chen <achen24@charlotte.edu>
2025-04-23 23:40:53 -0400

    updates test cases script

--- nbody-par/sequential/testcases.sh
+++ nbody-par/sequential/testcases.sh
@@ -8,21 +8,21 @@
 
 make all
 
-echo "---Test Case 1: 10 bodies, 1 step, 10,000 timesteps---" >> timing.txt
+echo "---Test Case 1: 5 bodies, dt=300s, 5,000,000 steps, print every 10,000---" >> timing.txt
 echo "Parallel version:" >> timing.txt
 ./nbody-par 5 300 5000000 10000 > sim-par-1.out 2>> timing.txt
 echo "Sequential version:" >> timing.txt
 ./nbody-seq 5 300 5000000 10000 > sim-seq-1.out 2>> timing.txt
 echo "" >> timing.txt
 
-echo "---Test Case 2: 50 bodies, 1 step, 5,000 timesteps---" >> timing.txt
+echo "---Test Case 2: 100 bodies, dt=300s, 50,000 steps, print every 500---" >> timing.txt
 echo "Parallel version:" >> timing.txt
 ./nbody-par 100 3000 50000 500 > sim-par-2.out 2>> timing.txt
 echo "Sequential version:" >> timing.txt
 ./nbody-seq 100 300 50000 500 > sim-seq-2.out 2>> timing.txt
 echo "" >> timing.txt
 
-echo "---Test Case 3: 100 bodies, 1 step, 2,000 timesteps---" >> timing.txt
+echo "---Test Case 3: Solar system, dt=200s, 5,000,000 steps, print every 10,000---" >> timing.txt
 echo "Parallel version:" >> timing.txt
 ./nbody-par planet 200 5000000 10000 > sim-par-3.out 2>> timing.txt
 echo "Sequential version:" >> timing.txt
